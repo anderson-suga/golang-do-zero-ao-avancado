d9f9533704f046663d95a06154e2d3fa5e1aebbe
Anderson Akihiro Suga <anderson.suga@gmail.com>
2022-12-12 19:05:24 -0300

    Create 04-variaveis.go

---
+++ secao-01/04-variaveis.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var peso int
+	peso = 10
+	fmt.Println(peso)
+
+	var mensagem1 string
+	mensagem1 = "10 é o peso"
+	fmt.Println(mensagem1)
+
+	mensagem2 := "Hello World!!!"
+	fmt.Println(mensagem2)
+}
